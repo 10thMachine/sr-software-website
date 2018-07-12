c12d620025b1cfbf080d933ae15167d6d42937a0
Jesstin Swadley <jesstinswadley1@gmail.com>
2018-07-12 11:09:26 -0700

    Testing the site out -Jesstin Swadley

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express 	= require('express'),
-	app 		= express(),
-	bodyParser 	= require('body-parser');
+const 	express 	= require('express'),
+		app 		= express(),
+		bodyParser 	= require('body-parser');
 
 // APP CONFIG
 app.set('view engine', 'ejs');
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // RESTFUL ROUTING
 app.get('/', function(req, res){
-	res.render('index')
+	res.render('index');
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("SR Software site is running"); 
+app.listen(3000, function(){
+	console.log("SR Software site is running");
 });
